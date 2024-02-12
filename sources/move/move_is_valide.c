2de4cbcd7432d05ba64c7fdd70386b2e9872d494
Luca Ray <luca.ray@protonmail.ch>
2024-02-12 14:48:24 +0100

    fix collisions

--- sources/move/move_is_valide.c
+++ sources/move/move_is_valide.c
@@ -6,7 +6,7 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:17:40 by lray              #+#    #+#             */
-/*   Updated: 2024/02/11 15:46:43 by lray             ###   ########.fr       */
+/*   Updated: 2024/02/12 14:45:11 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,14 @@ int	move_is_valide(t_ctx *ctx, double new_x, double new_y)
 	double	rounded_x;
 	double	rounded_y;
 
-	rounded_x = floor(new_x);
-	rounded_y = floor(new_y);
+	if (new_x > ctx->player.pos.x)
+		rounded_x = floor(new_x + 0.3);
+	else
+		rounded_x = floor(new_x - 0.3);
+	if (new_y > ctx->player.pos.y)
+		rounded_y = floor(new_y + 0.3);
+	else
+		rounded_y = floor(new_y - 0.3);
 	if (is_valide_new_x(ctx, rounded_x) && is_valide_new_y(ctx, rounded_y))
 	{
 		if (!is_wall(ctx, rounded_x, rounded_y))
@@ -47,7 +53,7 @@ static int	is_valide_new_y(t_ctx *ctx, double new_y)
 
 static int	is_wall(t_ctx *ctx, double new_x, double new_y)
 {
-	if (ctx->map->data[(int)new_x][(int)new_y] == 1)
+	if (ctx->map->data[(int)(new_y)][(int)(new_x)] == '1')
 		return (1);
 	return (0);
 }
