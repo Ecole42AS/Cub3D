44ce92fa99da22ebc4a8f479b0787432a5d6f335
Alexandre Stutz <astutz@pubfac.com>
2024-02-12 13:30:37 +0100

    Revert "fix parsing path and color"
    This reverts commit ed4da8c533fd65444265e106a97bd7444b29a237.

--- sources/parsing/colors.c
+++ sources/parsing/colors.c
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 19:31:21 by astutz            #+#    #+#             */
-/*   Updated: 2024/02/12 12:06:31 by astutz           ###   ########.fr       */
+/*   Updated: 2024/02/11 11:24:42 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,152 +15,73 @@
 /*◦ Si un problème de configuration de n’importe quel type est rencontré dans
 le fichier, le programme doit quitter et renvoyer "Error\n" suivi d’un message
 d’erreur explicite de votre choix.*/
+// void	parse_colors(int fd, t_color *color)
+// {
+// 	char	**split_result;
+// 	char	**split_colors;
+// 	char	*line;
 
-int is_valid_color_format(char *prefix) {
-    return (prefix[1] == '\0' && (prefix[0] == 'F' || prefix[0] == 'C'));
-}
+// 	line = gnl_unempty(fd);
+// 	split_result = ft_split(line, ' ');
+// 	split_colors = ft_split(split_result[1], ',');
+// 	if (!ft_strcmp(split_result[0], "F"))
+// 		color->rgb_floor = clr_make_trgb(0, ft_atoi(split_colors[0]), ft_atoi(split_colors[1]), ft_atoi(split_colors[2]));
+// 	free(line);
+// 	free_split(split_result);
+// 	free_split(split_colors);
+// 	line = gnl_unempty(fd);
+// 	split_result = ft_split(line, ' ');
+// 	split_colors = ft_split(split_result[1], ',');
+// 	if (!ft_strcmp(split_result[0], "C"))
+// 		color->rgb_ceiling = clr_make_trgb(0, ft_atoi(split_colors[0]), ft_atoi(split_colors[1]), ft_atoi(split_colors[2]));
+// }
 
-int parse_color_string(char *str) {
-    int r, g, b;
+static int extract_color_components(char *str, int *r, int *g, int *b) {
     char **split = ft_split(str, ',');
     if (!split)
-        return (-1);
-    if (split[0] && split[1] && split[2] && !split[3]) {
-        r = ft_atoi(split[0]);
-        g = ft_atoi(split[1]);
-        b = ft_atoi(split[2]);
-        if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255) {
-            free_split(split);
-            return clr_make_trgb(0, r, g, b);
-        }
+        return 1;
+    if (split[0] && split[1] && split[2]) {
+        *r = ft_atoi(split[0]);
+        *g = ft_atoi(split[1]);
+        *b = ft_atoi(split[2]);
+        free_split(split);
+        return 0;
     }
     free_split(split);
-    return -1;
+    return 1;
 }
 
-
-int assign_color(t_color *color, char *prefix, char *color_string)
-{
-    int result = parse_color_string(color_string);
-    if (result != 1)
-    {
-        if (!ft_strcmp(prefix, "F"))
-            color->rgb_floor = result;
-        else if (!ft_strcmp(prefix, "C"))
-            color->rgb_ceiling = result;
-        return (0);
-    }
-    else
-        return (1);
+static int parse_color_string(char *str) {
+    int r, g, b;
+    return !extract_color_components(str, &r, &g, &b) ? clr_make_trgb(0, r, g, b) : 1;
 }
 
-int parse_colors(int fd, t_color *color)
-{
+void parse_colors(int fd, t_color *color) {
     char *line;
     char **split_result;
     char **split_colors;
     int count;
-    int parsing_success = 0;
 
     count = -1;
-    while (++count < 2 && (line = gnl_unempty(fd)) != NULL) 
-    {
+    while (++count < 2 && (line = gnl_unempty(fd)) != NULL) {
         split_result = ft_split(line, ' ');
-        if (!split_result) 
-        {
-            free(line);
-            continue;
-        }
-        if (!is_valid_color_format(split_result[0])) 
-        {
-            parsing_success = 1;
-            free_split(split_result);
+        if (!split_result) {
             free(line);
             continue;
         }
         split_colors = ft_split(split_result[1], ',');
-        if (!split_colors) 
-        {
-            parsing_success = 1;
+        if (!split_colors) {
             free_split(split_result);
             free(line);
             continue;
         }
-        if(assign_color(color, split_result[0], split_result[1]))
-            parsing_success = 1;
+        if (!ft_strcmp(split_result[0], "F"))
+            color->rgb_floor = parse_color_string(split_result[1]);
+        else if (!ft_strcmp(split_result[0], "C"))
+            color->rgb_ceiling = parse_color_string(split_result[1]);
         free_split(split_result);
         free_split(split_colors);
         free(line);
     }
-
-    return parsing_success;
 }
 
-// int is_valid_color_format(char *prefix) {
-//     return (prefix[1] == '\0' && (prefix[0] == 'F' || prefix[0] == 'C'));
-// }
-
-// int parse_color_string(char *str) {
-//     int r, g, b;
-//     char **split = ft_split(str, ',');
-//     if (!split)
-//         return -1; // Erreur lors du split
-//     if (split[0] && split[1] && split[2] && !split[3]) { // Vérifier qu'il y a exactement trois composants RGB
-//         r = ft_atoi(split[0]);
-//         g = ft_atoi(split[1]);
-//         b = ft_atoi(split[2]);
-//         if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255) { // Vérifier que les composants sont dans la plage valide (0-255)
-//             free_split(split);
-//             return clr_make_trgb(0, r, g, b); // Générer la couleur RGB
-//         }
-//     }
-//     free_split(split);
-//     return -1; // Composants de couleur invalides
-// }
-
-// int assign_color(t_color *color, char *prefix, char *color_string) {
-//     int result = parse_color_string(color_string);
-//     if (result != -1) {
-//         if (!ft_strcmp(prefix, "F"))
-//             color->rgb_floor = result;
-//         else if (!ft_strcmp(prefix, "C"))
-//             color->rgb_ceiling = result;
-//         return 0;
-//     }
-//     return 1;
-// }
-
-// int parse_colors(int fd, t_color *color) {
-//     char *line;
-//     char **split_result;
-//     char **split_colors;
-//     int count;
-//     int parsing_success = 0;
-
-//     count = -1;
-//     while (++count < 2) {
-//         line = gnl_unempty(fd);
-//         if (!line)
-//             break;
-//         split_result = ft_split(line, ' ');
-//         free(line);
-//         if (!split_result)
-//             continue;
-//         if (!is_valid_color_format(split_result[0])) {
-//             free_split(split_result);
-//             continue;
-//         }
-//         split_colors = ft_split(split_result[1], ',');
-//         if (!split_colors) {
-//             free_split(split_result);
-//             continue;
-//         }
-//         parsing_success = assign_color(color, split_result[0], split_result[1]);
-//         free_split(split_result);
-//         free_split(split_colors);
-//         if (parsing_success)
-//             break;
-//     }
-
-//     return parsing_success;
-// }
--- sources/parsing/textures.c
+++ sources/parsing/textures.c
@@ -3,41 +3,150 @@
 /*                                                        :::      ::::::::   */
 /*   textures.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 09:21:50 by astutz            #+#    #+#             */
-/*   Updated: 2024/02/12 10:50:19 by astutz           ###   ########.fr       */
+/*   Updated: 2024/02/10 21:13:07 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-int parse_and_set_texture(t_texture *texture, int fd)
-{
+// Hello World
+
+// if (parse_texture_paths(..) == 1)
+// {
+// 	free(line);
+// 	free_split;
+// 	free_texture;
+// }
+
+// int	parse_texture_paths(t_texture *texture, int fd)
+// {
+//     char *line;
+//     char **split_result;
+
+//     line = gnl_unempty(fd);
+// 	if (!line)
+// 		return (1);
+//     split_result = ft_split(line, ' ');
+//     if (!ft_strcmp(split_result[0], "NO") && !split_result[2] && !split_result)
+// 		texture->no_texture_path = ft_strdup(split_result[1]);
+// 	else
+// 		return (1);
+//     free(line);
+//     free_split(split_result);
+
+// 	line = gnl_unempty(fd);
+// 	if (!line)
+// 		return (1);
+//     split_result = ft_split(line, ' ');
+//     if (!ft_strcmp(split_result[0], "SO") && !split_result[2] && !split_result)
+// 		texture->so_texture_path = ft_strdup(split_result[1]);
+// 	else
+// 		return (1);
+//     free(line);
+//     free_split(split_result);
+
+// 	line = gnl_unempty(fd);
+// 	if (!line)
+// 		return (1);
+//     split_result = ft_split(line, ' ');
+//     if (!ft_strcmp(split_result[0], "WE") && !split_result[2] && !split_result)
+// 		texture->we_texture_path = ft_strdup(split_result[1]);
+// 	else
+// 		return (1);
+//     free(line);
+//     free_split(split_result);
+
+// 	line = gnl_unempty(fd);
+// 	if (!line)
+// 		return (1);
+//     split_result = ft_split(line, ' ');
+//     if (!ft_strcmp(split_result[0], "EA") && !split_result[2] && !split_result)
+// 		texture->ea_texture_path = ft_strdup(split_result[1]);
+// 	else
+// 		return (1);
+//     free(line);
+//     free_split(split_result);
+// 	return (0);
+// }
+
+
+/* --------------------------     code to delete if not usefull    ------------------------------*/
+// void parse_and_set_texture_path(t_texture *texture, int fd, const char *identifier) {
+//     char *line = gnl_unempty(fd);
+//     char **split_result = ft_split(line, ' ');
+
+//     if (split_result && split_result[0] && !ft_strcmp(split_result[0], identifier)) {
+//         if (split_result[1]) {
+//             if (strcmp(identifier, "NO") == 0) texture->NO_texture_path = ft_strdup(split_result[1]);
+//             else if (strcmp(identifier, "SO") == 0) texture->SO_texture_path = ft_strdup(split_result[1]);
+//             else if (strcmp(identifier, "WE") == 0) texture->WE_texture_path = ft_strdup(split_result[1]);
+//             else if (strcmp(identifier, "EA") == 0) texture->EA_texture_path = ft_strdup(split_result[1]);
+//         }
+//     }
+
+//     free(line);
+//     free_split(split_result);
+// }
+
+// void parse_texture_paths(t_texture *texture, int fd) {
+//     parse_and_set_texture_path(texture, fd, "NO");
+//     parse_and_set_texture_path(texture, fd, "SO");
+//     parse_and_set_texture_path(texture, fd, "WE");
+//     parse_and_set_texture_path(texture, fd, "EA");
+// }
+
+
+
+ 	// if (!split_result || ft_strcmp(split_result[0], expected_name) != 0)
+    // {
+    //     perror("Erreur lors de la lecture ou du split de la ligne");
+    //     free_split(split_result);
+    //     return false;
+    // }
+
+	// if (!line || !split_result)
+	// {
+	// 	perror("Erreur lors de la lecture ou du split de la ligne");
+	// 	free(texture);
+	// 	free(line);
+	// 	free_split(split_result);
+	// 	close(fd);
+	// 	return 1;
+	// }
+
+// if (!process_texture_line(texture, line, texture_names[i], split_result[1]))
+// {
+// 	free(texture);
+// 	free(line);
+// 	free_split(split_result);
+// 	close(fd);
+// 	return 1;
+// }
+
+// ------------------------------Code refracto--------------------------------------
+int parse_and_set_texture(t_texture *texture, int fd, const char *prefix) {
     char *line = gnl_unempty(fd);
     if (!line)
         return 1;
+
     char **split_result = ft_split(line, ' ');
     free(line);
-    if ((!split_result 
-    || split_result[2])
-    && ft_strcmp(split_result[0], "SO") 
-    && ft_strcmp(split_result[0], "NO") 
-    && ft_strcmp(split_result[0], "EA") 
-    && ft_strcmp(split_result[0], "WE"))
-    {
+
+    if (!split_result || ft_strcmp(split_result[0], prefix) || split_result[2]) {
         free_split(split_result);
         return 1;
     }
 
-    texture_path_setter(texture, split_result[1], split_result[0]);
+    texture_path_setter(texture, split_result[1], prefix);
 
     free_split(split_result);
     return 0;
 }
 
-void texture_path_setter(t_texture *texture, const char *path, const char *prefix)
-{
+void texture_path_setter(t_texture *texture, const char *path, const char *prefix) {
     if (!strcmp(prefix, "NO"))
 	{
         texture->no_texture_path = ft_strdup(path);
@@ -61,15 +170,13 @@ void texture_path_setter(t_texture *texture, const char *path, const char *prefi
 	}
 }
 
-int parse_texture_paths(t_texture *texture, int fd)
-{
-    int i;
+int parse_texture_paths(t_texture *texture, int fd) {
+    const char *prefixes[] = { "NO", "SO", "WE", "EA" };
 
-    i = -1;
-    while (++i < 4)
-    {
-        if (parse_and_set_texture(texture, fd) != 0)
-            return (1);
+    for (int i = 0; i < (int)(sizeof(prefixes) / sizeof(prefixes[0])); ++i) {
+        if (parse_and_set_texture(texture, fd, prefixes[i]) != 0)
+            return 1;
     }
-    return (0);
+
+    return 0;
 }
